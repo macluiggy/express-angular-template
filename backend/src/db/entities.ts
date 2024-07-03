27edf1be0217747cf50d3f57fec87b487e131cca
macluiggy <luiggy.macias@imagineapps.co>
2024-07-03 09:12:51 -0500

    chore: Remove unnecessary files and code

--- backend/src/db/entities.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '../users/users.entity';
-import { SeederEntity } from './seeders.entity';
-
-export const entitiesObject = {
-  User,
-  SeederEntity,
-};
-export default [User, SeederEntity];
--- backend/src/lang/lang.type.ts
+++
@@ -1,9 +0,0 @@
-type Lang = {
-  USER: {
-    UPDATED: string;
-    ALREADY_EXISTS: string;
-    CREATED: string;
-  };
-};
-
-export default Lang;
