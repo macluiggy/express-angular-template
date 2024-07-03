f7ff69ce948f0180333ef37870d588e04363286e
macluiggy <luiggy.macias@imagineapps.co>
2024-07-03 10:19:42 -0500

    chore: Update backend dependencies and add API version constant

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,36 @@
 // src/server.ts
-import express from 'express';
-import { AppDataSource } from './infrastructure/database/data-source';
-import { UserRepository } from './infrastructure/repositories/UserRepository';
-import { UserService } from './application/services/UserService';
-import { UserController } from './infrastructure/controllers/UserController';
+import express from "express";
+import { AppDataSource } from "./infrastructure/database/data-source";
+import { UserRepository } from "./infrastructure/repositories/UserRepository";
+import { UserService } from "./application/services/UserService";
+import { UserController } from "./infrastructure/controllers/UserController";
+import { API_VERSION } from "./config/constants";
 
 const app = express();
 app.use(express.json());
 
-AppDataSource.initialize().then(() => {
-  const userRepository = new UserRepository();
-  const userService = new UserService(userRepository);
-  const userController = new UserController(userService);
+AppDataSource.initialize()
+  .then(() => {
+    const userRepository = new UserRepository();
+    const userService = new UserService(userRepository);
+    const userController = new UserController(userService);
 
-  app.get('/users', (req, res) => userController.getUsers(req, res));
-  app.get('/users/:id', (req, res) => userController.getUserById(req, res));
-  app.post('/users', (req, res) => userController.createUser(req, res));
-  app.put('/users/:id', (req, res) => userController.updateUser(req, res));
-  app.delete('/users/:id', (req, res) => userController.deleteUser(req, res));
+    // add a health check route
+    app.get("/health", (req, res) => {
+      res.send("Server is up and running");
+    });
 
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
-}).catch(error => console.log(error));
+    // add api/v1 prefix to all routes
+    // app.use(`/api/${API_VERSION}`, app);
+
+    app.get("api/v1/users", (req, res) => userController.getUsers(req, res));
+    app.get("/users/:id", (req, res) => userController.getUserById(req, res));
+    app.post("/users", (req, res) => userController.createUser(req, res));
+    app.put("/users/:id", (req, res) => userController.updateUser(req, res));
+    app.delete("/users/:id", (req, res) => userController.deleteUser(req, res));
+
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => console.log(error));
